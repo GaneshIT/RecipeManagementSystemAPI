465111c02b32a9e3fd97934d417d7cac3db81179
GaneshIT <ganesh.ceoit@gmail.com>
2025-03-14 02:59:53 +0530

    Add project files.

---
+++ RecipeManagementSystemAPI/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecipeManagementSystemAPI.Models;
+
+namespace RecipeManagementSystemAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Recipe> Recipes { get; set; }
+    }
+}
---
+++ RecipeManagementSystemAPI/Models/Recipe.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeManagementSystemAPI.Models
+{
+    public class Recipe
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Ingredients { get; set; }
+
+        [Required]
+        public string Instructions { get; set; }
+
+        public string Category { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
